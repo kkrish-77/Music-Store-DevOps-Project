f2974af15f0ed491ef3879125831971487aff8e5
kkrish-77 <kkrshsingh@gmail.com>
2025-11-13 13:37:14 +0530

    Update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "a93abb5d95df24e7aad53b0d059fe4ba-494d9850f21e6fc3.elb.ap-south-1.amazonaws.com"
+    ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
